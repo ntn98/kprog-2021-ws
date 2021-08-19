43d81fd10aef3949d4eb4a21bb50bfea753d126e
Joerg Winckler <winckler@hs-heilbronn.de>
2021-08-19 15:16:20 +0200

    some javadoc.

--- src/main/java/environment/testprograms/HelloWorld.java
+++ src/main/java/environment/testprograms/HelloWorld.java
@@ -1,5 +1,8 @@
 package environment.testprograms;
 
+/**
+ * Simple test program using the simple logger facade.
+ */
 public class HelloWorld {
   private static final org.slf4j.Logger logger =
           org.slf4j.LoggerFactory.getLogger(HelloWorld.class);
